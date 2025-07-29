7b3390bbd5202bb564f5b06809170991c29e660c
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-29 23:20:52 +0900

    fix append.sh to support additional import_custom directory

--- manage/bin/append.sh
+++ manage/bin/append.sh
@@ -4,37 +4,31 @@ shopt -s nullglob
 pthtop="$(cd "$(dirname "${0}")/../.." && pwd)"
 
 l=("${pthtop}"/import/*)
-
 for i in "${l[@]}"
 do
-  if test ! -e "${i}"/manage/cnf/latest.txt
+  if test -e "${i}/export"
   then
-    if test -e "${i}/export"
+    echo "export: ${i}"
+    cd "${i}/export"
+    l=(*)
+    if test ${#l[@]} -gt 0
     then
-      echo "export: ${i}"
-      cd "${i}/export"
-      l=(*)
-      if test ${#l[@]} -gt 0
-      then
-        tar -cf - * | (cd "${pthtop}"/export && tar -xf -)
-      fi
+      tar -cf - * | (cd "${pthtop}"/export && tar -xf -)
     fi
   fi
 done
 
+l=("${pthtop}"/import_custom/*)
 for i in "${l[@]}"
 do
-  if test -e "${i}"/manage/cnf/latest.txt
+  if test -e "${i}/export"
   then
-    if test -e "${i}/export"
+    echo "export: ${i}"
+    cd "${i}/export"
+    l=(*)
+    if test ${#l[@]} -gt 0
     then
-      echo "export: ${i}"
-      cd "${i}/export"
-      l=(*)
-      if test ${#l[@]} -gt 0
-      then
-        tar -cf - * | (cd "${pthtop}"/export && tar -xf -)
-      fi
+      tar -cf - * | (cd "${pthtop}"/export && tar -xf -)
     fi
   fi
 done
