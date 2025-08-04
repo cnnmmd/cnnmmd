b8c6b00b5da9a684ce1322364a11fbcb564c3c09
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-04 14:09:51 +0900

    resolve duplication in docker projects

--- manage/lib/shared.sh
+++ manage/lib/shared.sh
@@ -66,13 +66,14 @@ function delimg {
 function runimg {
   local cnfcmp=${1}
 
+  namprj=$(basename "${cnfcmp}" | sed -E 's/^docker_(.+)\.yml$/\1/')
   cnfusr="${cnfcmp%.*}_custom.${cnfcmp#*.}"
 
   if test -f "${cnfusr}"
   then
-    docker compose -f "${cnfcmp}" -f "${cnfusr}" up -d
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfusr}" up -d
   else
-    docker compose -f "${cnfcmp}" up -d
+    docker compose -p ${namprj} -f "${cnfcmp}" up -d
   fi
 }
 
@@ -81,13 +82,14 @@ function runimg {
 function endimg {
   local cnfcmp=${1}
 
+  namprj=$(basename "${cnfcmp}" | sed -E 's/^docker_(.+)\.yml$/\1/')
   cnfusr="${cnfcmp%.*}_custom.${cnfcmp#*.}"
 
   if test -f "${cnfusr}"
   then
-    docker compose -f "${cnfcmp}" -f "${cnfusr}" down
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfusr}" down
   else
-    docker compose -f "${cnfcmp}" down
+    docker compose -p ${namprj} -f "${cnfcmp}" down
   fi
 }
 
@@ -97,13 +99,14 @@ function runimg_opt {
   local cnfcmp=${1}
   local cnfopt=${2}
 
+  namprj=$(basename "${cnfcmp}" | sed -E 's/^docker_(.+)\.yml$/\1/')
   cnfusr="${cnfcmp%.*}_custom.${cnfcmp#*.}"
 
   if test -f "${cnfusr}"
   then
-    docker compose -f "${cnfcmp}" -f "${cnfopt}" -f "${cnfusr}" up -d
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfopt}" -f "${cnfusr}" up -d
   else
-    docker compose -f "${cnfcmp}" -f "${cnfopt}" up -d
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfopt}" up -d
   fi
 }
 
@@ -113,12 +116,13 @@ function endimg_opt {
   local cnfcmp=${1}
   local cnfopt=${2}
 
+  namprj=$(basename "${cnfcmp}" | sed -E 's/^docker_(.+)\.yml$/\1/')
   cnfusr="${cnfcmp%.*}_custom.${cnfcmp#*.}"
 
   if test -f "${cnfusr}"
   then
-    docker compose -f "${cnfcmp}" -f "${cnfopt}" -f "${cnfusr}" down
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfopt}" -f "${cnfusr}" down
   else
-    docker compose -f "${cnfcmp}" -f "${cnfopt}" down
+    docker compose -p ${namprj} -f "${cnfcmp}" -f "${cnfopt}" down
   fi
 }
